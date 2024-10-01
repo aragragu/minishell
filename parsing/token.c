df2f0a45aa0d130e9596bc1dd4ae71f4f9ed62dd
ANAS RAGRAGUI <101640895+aragragu@users.noreply.github.com>
2024-10-01 17:42:41 +0100

    for merging

--- parsing/token.c
+++ parsing/token.c
@@ -6,14 +6,12 @@
 /*   By: aragragu <aragragu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 10:44:37 by aragragu          #+#    #+#             */
-/*   Updated: 2024/09/30 18:33:36 by aragragu         ###   ########.fr       */
+/*   Updated: 2024/10/01 17:41:39 by aragragu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-//export var ="cat Makefile | grep >"
-//export "test=ici"=coucou
-//c$var Makefile
+
 void read_input(char **env)
 {
 	t_elem *list = NULL;
@@ -50,10 +48,8 @@ void read_input(char **env)
 		}
 		expand_var_list(&list, var, &garbage);
 		concatination(&list, &garbage);
-		print_list(&list);
 		handle_redirection(&list, &var.env, &garbage);
 		import_data(&var.list, &list, &garbage);
-		print_cmd(var.list);
 		execution(&var);
 		free_garbage(&garbage);
 		list = NULL;
@@ -166,7 +162,7 @@ void is_a_var(t_elem **list, char *input, int index, t_garbage **garbage)
 	}
 	else
 	{
-		while (input[index + 1 + len] && (is_alphanumeric(input[index + 1 + len]) || input[index + 1 + len] == '_' || input[index + 1 + len] == '?'))
+		while (input[index + 1 + len] && (is_alphanumeric(input[index + 1 + len]) || input[index + 1 + len] == '_'))
 			len++;
 		str = ft_substr(input, index, len + 1, garbage);
 		if (ft_strlen(str) == 1)
