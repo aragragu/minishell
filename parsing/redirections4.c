e3ecb01567348eccd7202de3e3e4ef0f38557d95
ANAS RAGRAGUI <101640895+aragragu@users.noreply.github.com>
2024-10-22 16:15:06 +0100

    fixed herdoc leasks

--- parsing/redirections4.c
+++ parsing/redirections4.c
@@ -6,7 +6,7 @@
 /*   By: aragragu <aragragu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 19:38:59 by aragragu          #+#    #+#             */
-/*   Updated: 2024/10/22 13:01:40 by aragragu         ###   ########.fr       */
+/*   Updated: 2024/10/22 16:12:26 by aragragu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,12 @@ int	check_herdoc_line(t_elem **list, t_herdoc *data, t_garbage **garbage)
 		open_herdoc_file(data);
 		return (1);
 	}
+	ft_lstadd_back_garbage(garbage, ft_lstnew_garbage(data->line));
 	if (!ft_strcmp((*list)->content, data->line))
 	{
 		open_herdoc_file(data);	
 		return (1);
 	}
-	if (!*data->line)
-	{
-		puts("test");
-		free(data->line);
-		return (2);
-	}
-	ft_lstadd_back_garbage(garbage, ft_lstnew_garbage(data->line));
 	return (0);
 }
 
